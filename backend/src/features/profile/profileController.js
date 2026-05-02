7bcfa095103be30bb559d4b8f7dde4e68c7407bd
Mohab Khaled <mohabk.2006@gmail.com>
2026-05-02 19:00:35 +0300

    Add password length validation in profile controller

--- backend/src/features/profile/profileController.js
+++ backend/src/features/profile/profileController.js
@@ -70,6 +70,13 @@ const changeMyPassword = async (req, res) => {
 			});
 		}
 
+		if (newPassword.length < 6) {
+    		return res.status(400).json({ 
+				success: false,
+				message: "Password must be at least 6 characters" 
+			});
+		}
+
 		const user = await User.findById(req.user._id);
 
 		if (!user) {
