ebee54e07db5d12dca3a23fb195e510c8610eee7
ZeyadElserwy_16005823 <zeyad.elserwy@student.giu-uni.de>
2026-05-06 15:52:21 +0300

    Update JobPost.js

--- backend/src/features/job-posts/JobPost.js
+++ backend/src/features/job-posts/JobPost.js
@@ -1,4 +1,4 @@
-// JobPost Model | Jobs: Browse & Detail
+// JobPost Model | Jobs: Browse & Detail.
 const mongoose = require("mongoose");
 
 const JOB_TYPES = ["full-time", "part-time", "internship"];
