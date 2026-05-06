5287cc07d881bbab56e63d498915250c63170d30
ZeyadElserwy_16005823 <zeyad.elserwy@student.giu-uni.de>
2026-05-06 15:52:35 +0300

    Fix comment formatting in jobController.js
    Updated comment for job routes to include a period.

--- backend/src/features/job-posts/jobController.js
+++ backend/src/features/job-posts/jobController.js
@@ -1,4 +1,4 @@
-//Jobs: Browse & Detail (GET /jobs, GET /jobs/:id, GET /jobs/my-jobs)
+//Jobs: Browse & Detail (GET /jobs, GET /jobs/:id, GET /jobs/my-jobs).
 const JobPost = require("./JobPost");
 
 // GET /api/v1/jobs
