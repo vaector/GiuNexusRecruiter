6dbe7b4790ae4817fe8be0bc34656e8d4df3a28b
gasTSK <taher.skhalaf@gmail.com>
2026-05-07 12:20:50 +0300

    refix mohab revert 😭😭😡😡💯👍

--- backend/src/features/profile/profileRoutes.js
+++ backend/src/features/profile/profileRoutes.js
@@ -1,7 +1,16 @@
 const express = require("express");
 
+const { protect } = require("../../middleware/auth");
+const {
+	getMyProfile,
+	updateMyProfile,
+	changeMyPassword,
+} = require("./profileController");
+
 const router = express.Router();
 
-// TODO: mount profile routes
+router.get("/", protect, getMyProfile);
+router.patch("/", protect, updateMyProfile);
+router.patch("/change-password", protect, changeMyPassword);
 
 module.exports = router;
