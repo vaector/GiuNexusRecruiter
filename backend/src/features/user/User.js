b2237956d3959891be81bd1b3da2552742f3eed1
MohabHindawy <mohab.hindawy@student.giu-uni.de>
2026-05-22 11:34:30 +0300

    reverted the next

--- backend/src/features/user/User.js
+++ backend/src/features/user/User.js
@@ -186,6 +186,7 @@ UserSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
   this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
 UserSchema.methods.comparePassword = function (candidatePassword) {
